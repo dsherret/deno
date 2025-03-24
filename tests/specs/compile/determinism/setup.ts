8525eae9f153e6bca63c286fae34b3bbc34213b4
David Sherret <dsherret@gmail.com>
2025-03-24 11:47:27 -0400

    maybe fix mac

--- tests/specs/compile/determinism/setup.ts
+++
@@ -1,5 +0,0 @@
-// for setup, we create two directories with the same file in each
-// and then when compiling we ensure this directory name has no
-// effect on the output
-Deno.mkdirSync("a");
-Deno.copyFileSync("main.ts", `a/main.ts`);
