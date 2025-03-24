46840d2f04bda24e05d1b559f86fa7d1049915fe
David Sherret <dsherret@gmail.com>
2025-03-24 11:45:10 -0400

    maybe fix mac ci

--- tests/specs/compile/determinism/setup.ts
+++ tests/specs/compile/determinism/setup.ts
@@ -1,10 +1,5 @@
 // for setup, we create two directories with the same file in each
 // and then when compiling we ensure this directory name has no
 // effect on the output
-makeCopyDir("a");
-makeCopyDir("b");
-
-function makeCopyDir(dirName) {
-  Deno.mkdirSync(dirName);
-  Deno.copyFileSync("main.ts", `${dirName}/main.ts`);
-}
+Deno.mkdirSync("a");
+Deno.copyFileSync("main.ts", `a/main.ts`);
